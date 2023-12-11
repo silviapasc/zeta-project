ac22a79334a17f41a9bbbddf3c478ec2618a3e72
Silvia Pasciullo <pasciullo.s@gmail.com>
2023-12-11 14:33:39 +0100

    test_define_dictionary() defined

--- tests/test_zeta.py
+++ tests/test_zeta.py
@@ -1,4 +1,4 @@
-from ken_project.zeta import (set_cwd, read_text)
+from ken_project.zeta import (set_cwd, read_text, define_dictionary)
 import pytest
 import os
 import pandas as pd
@@ -25,5 +25,24 @@ def test_read_text():
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(file_content)
 
-    # Call the function and check if returned and expected content match
+    # Call the function under test and check if returned and expected content match
     assert read_text(filename) == file_content
+
+
+def test_define_dictionary(tmp_path):
+    # Create some test files
+    file1 = tmp_path / "file1.txt"
+    file1.write_text("This is a text file")
+    file2 = tmp_path / "file2.txt"
+    file2.write_text("This is another text file")
+    file3 = tmp_path / "file3.csv"
+    file3.write_text("This is not a text file, but a csv file")
+
+    # Call the function under test
+    result = define_dictionary(tmp_path)
+
+    # Assert the expected dictionary
+    assert result == {"file1.txt": "This is a text file", "file2.txt": "This is another text file"}
+
+
+
