6bd3872cf7bee9eb5e81fd02a139edc21fdcd817
Silvia Pasciullo <pasciullo.s@gmail.com>
2023-12-11 12:38:05 +0100

    test_set_cwd() added

--- tests/test_zeta.py
+++ tests/test_zeta.py
@@ -1,2 +1,16 @@
-def test_set_cwd():
-    assert False
+from ken_project.zeta import (set_cwd)
+import pytest
+import os
+import pandas as pd
+
+
+# Test set_cwd() using the tmp_path fixture, which provides a temporary
+# directory unique to the test invocation
+def test_set_cwd(tmp_path):
+    # Test when the specified path is equal to os.getcwd()
+    current_path = os.getcwd()
+    assert set_cwd(current_path) == current_path
+
+    # Test when the specified path is different from os.getcwd()
+    new_path = tmp_path
+    assert set_cwd(new_path) == os.chdir(new_path)
