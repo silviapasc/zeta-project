0603d738ede3f9110e788c388c2b7d0a5fe0b677
Silvia Pasciullo <pasciullo.s@gmail.com>
2023-12-11 13:19:47 +0100

    test_read_text() defined

--- tests/test_zeta.py
+++ tests/test_zeta.py
@@ -1,4 +1,4 @@
-from ken_project.zeta import (set_cwd)
+from ken_project.zeta import (set_cwd, read_text)
 import pytest
 import os
 import pandas as pd
@@ -14,3 +14,16 @@ def test_set_cwd(tmp_path):
     # Test when the specified path is different from os.getcwd()
     new_path = tmp_path
     assert set_cwd(new_path) == os.chdir(new_path)
+
+
+def test_read_text():
+    # Define a filename and its corresponding content as variables
+    filename = "test_file.txt"
+    file_content = "Test file content! @ #123"
+
+    # Create/write a test file with the given content
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(file_content)
+
+    # Call the function and check if returned and expected content match
+    assert read_text(filename) == file_content
