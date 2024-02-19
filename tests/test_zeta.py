bf92455c147ccc74d7710fe742288a91eeb34b39
Silvia Pasciullo <pasciullo.s@gmail.com>
2024-02-19 14:12:17 +0100

    Variable name updated

--- tests/test_zeta.py
+++ tests/test_zeta.py
@@ -90,10 +90,10 @@ def test_create_df_duplicate_file_names():
     corpus_dict = {'file1.txt': 'This is file #1.', 'file1.txt': 'This is a duplicate file #1.'}
 
     # Raise a ValueError exception in case of duplicate file names
-    with pytest.raises(Exception) as e:
+    with pytest.raises(Exception) as exception:
         # In the block, call the function that is expected to fail
         create_df(corpus_dict)
-        assert e.type == ValueError
+        assert exception.type == ValueError
 
 
 # Test case for lowercase() function
